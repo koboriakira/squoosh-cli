494b3661204fb403e8690ada74ad1add4383016b
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-07-22 14:25:43 +0900

    スクリプトを一部修正

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 # docker build -t squoosh-cli .
-# docker run -it --rm squoosh-cli squoosh-cli --help
+# docker run -it --rm squoosh-cli test.jpg
 FROM node:14.19.0
 
 RUN npm install -g @squoosh/cli
-
-# squoosh-cli --mozjpeg '{quality:30}' 画像サンプル_大きめ.jpg
--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # 第1引数に指定されたファイルを、squoosh-cliで圧縮する
 
+# 環境変数WORKDIRが設定されていない場合は、デフォルト値を設定
+if [ -z $WORKDIR ]; then
+  WORKDIR=/tmp/squoosh
+fi
 
 # 第1引数が指定されていない場合はエラー
 if [ $# -ne 1 ]; then
@@ -14,7 +18,6 @@ if [ ! -e $1 ]; then
   exit 1
 fi
 
-WORKDIR=$HOME/git/squoosh-cli/workdir
 # 第1引数のファイルパスについて、ディレクトリ部分を除いてファイル名を取得
 DIR_NAME=$(cd $(dirname $1); pwd)
 FILE_NAME=$(basename $1)
@@ -25,8 +28,9 @@ if [ ! -d $WORKDIR ]; then
 fi
 
 # 指定されたファイルをworkdirにコピー
-echo "copy $DIR_NAME/$FILE_NAME to $WORKDIR/$FILE_NAME"
-cp $DIR_NAME/$FILE_NAME $WORKDIR/$FILE_NAME
+WORK_FILE_PATH=$WORKDIR/$FILE_NAME
+echo "copy $DIR_NAME/$FILE_NAME to $WORK_FILE_PATH"
+cp $DIR_NAME/$FILE_NAME $WORK_FILE_PATH
 
 # 変換
 docker run -it --rm -v $WORKDIR:/var squoosh-cli squoosh-cli --mozjpeg '{quality:30}' -d /var /var/$FILE_NAME
@@ -37,5 +41,5 @@ echo $original_file_path
 file_path=${original_file_path%.*}_squoosh
 ext=${original_file_path##*.}
 copy_file_path=$file_path.$ext
-echo "move $WORKDIR/$FILE_NAME to $copy_file_path"
-mv -f $WORKDIR/$FILE_NAME $copy_file_path
+echo "move $WORK_FILE_PATH to $copy_file_path"
+mv -f $WORK_FILE_PATH $copy_file_path
